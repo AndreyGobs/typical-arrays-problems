41fb736d2aebada6869121d8b4b4a21c29ea818e
Andrey <55401672+AndreyGobs@users.noreply.github.com>
2021-03-10 00:24:30 +0300

    feat: add min max avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+   if (array === undefined || array === null || array.length <= 0) {
+      return 0;
+    } else {
+      let minN = array.sort((a, b) => a - b);
+      return minN[0];
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array === null || array.length <= 0) {
+      return 0;      
+    } else {
+      let maxN = array.sort((a, b) => b - a);
+      return maxN[0];
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array === null || array.length <= 0) {
+      return 0;
+  } else {
+      let sum = array.reduce((sum, current) => sum + current, 0);
+      let avg = sum / array.length;
+      return avg;
+  }
 }
